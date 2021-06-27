6da977fa23a50c917c30d41cc56de56b6ca6ad13
George U <petegeorge20005@gmail.com>
2021-06-27 17:49:04 +0100

    Added login endpoint

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 "use strict";
 var http = require('http');
+var url = require('url');
 
 console.log('Server running at http://127.0.0.1:8080/');
 
@@ -8,5 +9,11 @@ database.initDatabase();
 
 http.createServer(function (_request, response) {
   response.writeHead(200, {'Content-Type': 'text/html'});
+
+  switch(_request.url){
+    case '/login':
+      break;
+  }
+
   response.end('Hello World');
 }).listen(8080);
