1f8a34dc97817db01aad4e18b36f186a96802701
UcGeorge <petegeorge20005@gmail.com>
2021-08-24 09:45:39 +0100

    Added extra error management

--- api/controllers/sessionController.js
+++ api/controllers/sessionController.js
@@ -6,21 +6,20 @@ var Fuel = require('../models/fuelModel');
 
 exports.get = function(req, res) {
     console.log("[GET] /sessions");
-    User.authenticate(req.body.token, function (err, result){
-        if (err){
+    User.authenticate(req.body.token, function(err, result) {
+        if (err) {
             console.log(`   |__ Error: ${err}`);
             res.send(err)
-        }
-        else if(result){
-            Session.get(req.body.username, function (err, result){
-                Fuel.get(function (err, fuelresult){
-                    res.status(200).json({sessions : result, fuel : fuelresult})
+        } else if (result) {
+            Session.get(req.body.username, function(err, result) {
+                Fuel.get(function(err, fuelresult) {
+                    res.status(200).json({ sessions: result, fuel: fuelresult })
                 })
             })
-        }else{
+        } else {
             console.log(`   |__ Unauthorized!!`);
             res.status(401).json({
-                message:"Unauthorized"
+                message: "Unauthorized"
             })
         }
     });
@@ -28,26 +27,27 @@ exports.get = function(req, res) {
 
 exports.put = function(req, res) {
     console.log("[PUT] /sessions");
-    User.authenticate(req.body.token, function (err, result){
-        if (err){
+    User.authenticate(req.body.token, function(err, result) {
+        if (err) {
             console.log(`   |__ Error: ${err}`);
-            res.send(err)
-        }
-        else if(result){
+            res.status(401).json({
+                message: "Unauthorized: " + err,
+            })
+        } else if (result) {
             let sessionObject = {
-                username : req.body.username,
-                fuel_ID : req.body.fuel,//parseInt(req.body.fuel, 10),
-                distance : req.body.distance,
-                emission_quantity : req.body.emission_quantity
-            }
-            // console.log(sessionObject)
-            Session.put(req.body.username, sessionObject, function (err, result){
+                    username: req.body.username,
+                    fuel_ID: req.body.fuel, //parseInt(req.body.fuel, 10),
+                    distance: req.body.distance,
+                    emission_quantity: req.body.emission_quantity
+                }
+                // console.log(sessionObject)
+            Session.put(req.body.username, sessionObject, function(err, result) {
                 res.status(200).json(result)
             })
-        }else{
+        } else {
             console.log(`   |__ Unauthorized!!`);
             res.status(401).json({
-                message:"Unauthorized"
+                message: "Unauthorized"
             })
         }
     });
